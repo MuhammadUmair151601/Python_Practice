d3bbbe46583e5f64f63942abed9c72a59251287a
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-05-20 15:00:00 +0500

    strings slicing done

---
+++ 03_strings.py
@@ -0,0 +1,20 @@
+# a normal string is printed as below:
+# name = "muhammad umair"
+# print(name)
+
+# now if we want to do string slicing:
+# nameSlice = name[0:8] 
+# it will print the elements from 0 to 7 8 is excluded.
+# print(nameSlice)
+
+# negative indexes works as its negative numbering starts as -1 from the last one character:
+# slicing with skip value states as
+# [0:6:2]
+# print 0 to 5 and skip every 2nd value of the string:
+
+# Example:
+# sample = "Hello everyone how are you all"
+# sampleSkip = sample[0:12:2]
+# print(sampleSkip)
+# [:8] will print from 0 to 7 string:
+# [3:] will print from 3 to end:
