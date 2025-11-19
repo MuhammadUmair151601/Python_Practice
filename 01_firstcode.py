6acaa3ebbd53b97f5d93eddc9a5999e8d46968bd
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-11-19 14:47:55 +0500

    chapter 1 done with problems.

---
+++ 01_firstcode.py
@@ -0,0 +1,41 @@
+# print("Hello world")
+
+#single line comments.
+'''multiple
+line comments'''
+
+# Practice problem #1
+# write a python program to print twinkle twinkle little star poem:
+
+# print(""" twinkle twinkle little star
+#  how are wonder what you are """)
+
+# practice problem #2
+# use REPL and print the table of 5 using it:
+# open cli and enter python then print table of 5:
+
+# practice problem # 3
+# install a module of your own interest and practice a problem on it.
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("Hello Umair, pyttsx3 is working.")
+# engine.runAndWait()
+# print("Done speaking.")
+
+# Practice problem # 4
+# write a python program to print the contents of the directory of the module us the os module.
+# import os
+
+# # Get the directory where this Python file is located
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# print("Current module directory:", current_dir)
+# print("\nContents of the directory:")
+
+# # List and print each item in that directory
+# for item in os.listdir(current_dir):
+#     print(item)
+
+# Practice problem  # 5
+# Label the program written in problem 4 with comments.
+# already commented.
