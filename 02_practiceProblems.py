ff42a6b0d4e92343b1705fc65db10ec71e092dfc
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-11-19 15:21:03 +0500

    ch 02 practice done

---
+++ 02_practiceProblems.py
@@ -0,0 +1,27 @@
+# write a python problem to print the sum of two numbers
+# a = 12
+# b = 12
+# print(a+b)
+
+# write a python program to find the remainder when the number is divided by z/
+# a = 25
+# b = 5
+# print("the remainder of the number is :", a%b)
+
+# check the type of a variable assigned using input() function:
+# a = "ALI Ahmed"
+# print(type(a))
+
+# use comparison operator to find out whether 'a' given variable a is greater than variable b:
+# a = 34
+# b = 80
+# print(a>b)
+
+# write a python problem to find an average of two numbers entered by the user.
+# a = 12
+# b = 12
+# print((a+b)/2)
+
+#  write a python program to calculate the square of a number entered by the user.
+# a = int(input("enter a number to find the square"))
+# print(a*a)
