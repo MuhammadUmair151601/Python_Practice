e53bf4cedd3ab0364807958c1429696d11269047
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-11-19 15:05:53 +0500

    var and data types done

---
+++ 02_var&dataTypes.py
@@ -0,0 +1,31 @@
+# variable and datatypes:
+# a = 1
+# b = 2.2
+# c = 3
+# print(a+b+c)
+# python itself recognizes the datatype.
+# a = 25
+# b = a
+# print(b)
+
+# a = 10
+# b = 10
+# print(a == b)
+# it will return boolean true or false
+
+# Type() function defines the data type of the element:
+# a = 2.1
+# b = 2
+# c = 'ali'
+# print(type (a))
+# print(type (b))
+# print(type (c))
+# result will be float:
+
+# input() function used to take input from the user:
+# Example:
+# a = int(input("enter the value of a"))
+# b = int(input("enter the value of b"))
+# # result = a + b
+# print("the sum of your numbers is :", a + b)
+# int is used because the digits entered are considered as strings int converts them into integer:
