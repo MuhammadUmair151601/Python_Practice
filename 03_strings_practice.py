74c62f4ce59a1689f916c5c148cce8bc8ae6c34b
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-11-21 17:03:47 +0500

    chapter 4 problems done

---
+++ 03_strings_practice.py
@@ -0,0 +1,30 @@
+# Problem 1
+# write a python programto display a user entered name followed by good morning:
+# user = input("enter the user name to be greeted:")
+# print("Good afternoon dear",user)
+
+# Problem 2
+# replace the name and date in the following code:
+
+# letter = ''' dear |<name>|,
+# you are selecteds !
+# |<date>|'''
+# newletter = letter.replace("|<name>|","umair") .replace("|<date>|", "21 oct 2025")
+# print(newletter)
+
+# Problem 3 
+# write a python program to detect double space in a string:
+# name = "muhammad  umair"
+# spacefind = name.find("  ")
+# print(spacefind)
+
+# Problem 4
+# replace the double space from problem 3 with single space
+# name = "muhammad  umair"
+# spacereplace = name.replace("  "," ")
+
+# print(spacereplace)
+
+# write a python program to use escape sequence:
+# print("hello everyone \n" 
+# "i am fine and how about you")
